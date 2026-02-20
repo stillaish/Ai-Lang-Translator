45c3beff69021109c9ee92ff08ad24c767a6f76c
stillaish <bossbm05@gmail.com>
2026-02-20 20:11:30 +0530

    project completed

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask 
+from flask import Flask, render_template, request
+from transformers import MarianMTModel, MarianTokenizer
 app = Flask(__name__)
-@app.route('/')
+
+model_name = "Helsinki-NLP/opus-mt-en-hi"
+model = MarianMTModel.from_pretrained(model_name)
+tokenizer = MarianTokenizer.from_pretrained(model_name)
+
+def translation(data):
+    inputs = tokenizer(data, return_tensors="pt", padding=True)
+    translated = model.generate(**inputs)
+    result = tokenizer.decode(translated[0], skip_special_tokens=True)
+    return result
+   
+@app.route('/', methods=['GET','POST'])
 def index():
-    return "language translator"
+    translated_text = ""
+    if request.method == 'POST':
+        data = request.form['data']
+        translated_text = translation(data)
+    return render_template("index.html", translated_text=translated_text)
 if __name__ == '__main__':    
     app.run(debug=True)
